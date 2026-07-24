c8048d61dad0803e0450c54c3a2e2182896c42da
RACHIT KALE <rachitkale05@gmail.com>
2026-07-24 17:08:10 +0530

    added cors to the server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const path = require("path");
-require("dotenv").config({path: path.resolve(__dirname, "../../.env")});
+const cors = require('cors');
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 const apiRoutes = require("./routes/api.js")
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db.js");
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'], // Allow your React Vite dev server
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow custom auth headers
+    credentials: true
+}));
 
 app.use(express.json());
 
@@ -16,5 +24,5 @@ connectDB();
 app.use("/api/v1", apiRoutes)
 
 app.listen(PORT, (err) => {
-    err ? console.log("== ERROR STARTING SERVER ==", err) : console.log("Server Started on PORT:",PORT)
+    err ? console.log("== ERROR STARTING SERVER ==", err) : console.log("Server Started on PORT:", PORT)
 })
